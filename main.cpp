bf8eed1f0a909037757ab8eb29b18b30cfb7368a
Siege <81945479+SiegeToaster@users.noreply.github.com>
2021-12-03 17:12:39 -0800

    updated to (mostly) empty project
    add include directory: /Qt/6.2.2/mingw_64/include/**

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include <QPushButton>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+int main(int argc, char **argv) {
+ QApplication app (argc, argv);
+
+ return app.exec();
 }
+
+// https://wiki.qt.io/Qt_for_Beginners
