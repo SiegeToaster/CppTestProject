d3bb8e01e383b3fa0b96f22b2f4c5a193ca4d2a5
SiegeToaster <81945479+SiegeToaster@users.noreply.github.com>
2021-12-06 13:14:24 -0800

    added button; bigger window size; started classes

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 
 int main(int argc, char **argv) {
  QApplication app (argc, argv);
+ QWidget window;
+ window.setFixedSize(720, 480);
+
+ QPushButton *button = new QPushButton("wee woo", &window);
+ QPushButton *button2 = new QPushButton("other", &window);
+ button->setGeometry(360-40, 240-15, 80, 30); // 1st is windowWidth/2 - buttonWidth/2; 2nd is same but with height
+ button2->setGeometry(400, 240-15, 80, 30);
+ window.show();
 
  return app.exec();
 }
