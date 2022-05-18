1020a65aeb20c0e44ae18f1ad59fb3088349cfb4
SiegeToaster <81945479+SiegeToaster@users.noreply.github.com>
2022-05-17 17:52:16 -0700

    curl addition

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -4,23 +4,24 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <curl\curl.h>
 
 using namespace std;
 
-#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib,"ws2_32.lib")
 
-vector<pair<string, int>> parse(const string& str) {
+vector<pair<string, bool>> parse(const string& str) {
 	bool started = false;
 	bool inQuotes = false;
 	string nextStr;
 	unsigned long nextInt = 0;
-	vector<pair<string, int>> result;
+	vector<pair<string, bool>> result;
 
 	// For each character in the string
 	for (string::const_iterator it = str.begin() + 1 /* skip 1st open bracket */; it != str.end() - 1 /* skip last closing bracket */; it++) {
 		/*
 		* Wait for first '[', start vector
-		* Then seperate each nested array (always 2 elements), [0] = string literal, [1] = integer
+		* Then separate each nested array (always 2 elements), [0] = string literal, [1] = integer
 		* Wait for a comma, start a new vector
 		* wait for the closing ']', return
 		*/
@@ -48,7 +49,7 @@ vector<pair<string, int>> parse(const string& str) {
 		}
 		else {
 			if (*it == ',') {
-				result.push_back(make_pair(nextStr, nextInt));
+				result.push_back(make_pair(nextStr, nextInt /* Auto converted to bool (not 0 is true) */));
 				nextInt = 0;
 			}
 			else if (*it == '[') {
@@ -62,14 +63,23 @@ vector<pair<string, int>> parse(const string& str) {
 	return result;
 }
 
+int httpTest(string url, string other) {
+	
+}
+
 int main()
 {	
-	const string input = "[[\"berb8\",10],[\"Umbermoon\",0],[\"[DELETED]\",2],[\"Grant \",1],[\"Siege\",0],[\"PotionCast\",1],[\"McKendrick\",0]]";
-	vector<pair<string, int>> inputVec = parse(input);
+	const string input = "[[\"berb8\",70],[\"Umbermoon\",0],[\"[DELETED]\",2],[\"Grant \",1],[\"Siege\",0],[\"PotionCast\",1],[\"McKendrick\",0]]";
+	vector<pair<string, bool>> inputVec = parse(input);
+	/*
 	for (int i = 0; i < inputVec.size(); i++) {
 		cout << inputVec.at(i).first;
 		cout << ':';
-		cout << inputVec.at(i).second;
+		cout << !inputVec.at(i).second;
 		cout << '\n';
 	}
+	*/
+	string host = "sheets.googleapis.com";
+	string otherBS = "/v4/spreadsheets/1jz9qTfvp5hRZGuyN7HidmstlfEkVtcUe7kynWi0bUlY/values:batchGet?ranges=Medal%20Hall!A3%3AD&key=AIzaSyBABnhAmIU_u2gitsTZWC33bNteka64-5o";
+	return httpTest(host, otherBS);
 }
